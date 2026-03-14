933feeaf50da24c7401fcaa290853706094a5215
Farhan Farooqui <farhanfarooqui2099@gmail.com>
2026-03-14 11:59:59 +0600

    add viewModelKey to prevent stale view model; fix ViewModelProvider

--- lib/viewmodel_builder.dart
+++ lib/viewmodel_builder.dart
@@ -2,11 +2,13 @@ import 'package:flutter/widgets.dart';
 import 'package:viewmodel_flutter/flutter_viewmodel.dart';
 
 class ViewModelBuilder<T extends ViewModel> extends StatefulWidget {
+  final Object? viewModelKey;
   final T Function() viewModelBuilder;
   final Widget Function(BuildContext context, T viewModel) builder;
 
   const ViewModelBuilder({
     super.key,
+    this.viewModelKey,
     required this.viewModelBuilder,
     required this.builder,
   });
@@ -22,22 +24,39 @@ class _ViewModelBuilderState<T extends ViewModel>
   late T viewModel;
   late VoidCallback _listener;
 
+  void _createAndBindViewModel() {
+    viewModel = widget.viewModelBuilder();
+    viewModel.init();
+    viewModel.addListener(_listener);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      viewModel.update();
+    });
+  }
+
+  void _disposeViewModel() {
+    viewModel.removeListener(_listener);
+    viewModel.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
-    viewModel = widget.viewModelBuilder();
-
-    viewModel.init();
     _listener = () {
       if (mounted) {
         setState(() {});
       }
     };
-    viewModel.addListener(_listener);
+    _createAndBindViewModel();
     WidgetsBinding.instance.addObserver(this);
-    WidgetsBinding.instance.addPostFrameCallback((duration) {
-      viewModel.update(null);
-    });
+  }
+
+  @override
+  void didUpdateWidget(covariant ViewModelBuilder<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.viewModelKey != widget.viewModelKey) {
+      _disposeViewModel();
+      _createAndBindViewModel();
+    }
   }
 
   @override
@@ -49,8 +68,7 @@ class _ViewModelBuilderState<T extends ViewModel>
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
-    viewModel.removeListener(_listener);
-    viewModel.dispose();
+    _disposeViewModel();
     super.dispose();
   }
 
@@ -58,36 +76,46 @@ class _ViewModelBuilderState<T extends ViewModel>
   Widget build(BuildContext context) {
     return ViewModelProvider<T>._create(
       notifier: viewModel,
-      child: widget.builder(context, viewModel),
+      child: Builder(
+        builder: (providerContext) {
+          return widget.builder(providerContext, viewModel);
+        },
+      ),
     );
   }
 }
 
-class ViewModelProvider<T extends ViewModel> extends InheritedNotifier {
-  // final T viewModel;
+class ViewModelProvider<T extends ViewModel> extends InheritedNotifier<T> {
   const ViewModelProvider._create({
     super.key,
     required super.notifier,
     required super.child,
   });
 
-  static T of<T extends ViewModel>(BuildContext context) {
-    final viewModel = context
+  static T? maybeOf<T extends ViewModel>(BuildContext context) {
+    return context
         .dependOnInheritedWidgetOfExactType<ViewModelProvider<T>>()
         ?.notifier;
-
-    assert(viewModel != null, "No appropriate ViewModel found in the context.");
-
-    return viewModel! as T;
   }
 
-  @override
-  bool updateShouldNotify(covariant InheritedNotifier<Listenable> oldWidget) {
-    return notifier != oldWidget.notifier;
+  static T of<T extends ViewModel>(BuildContext context) {
+    final viewModel = maybeOf<T>(context);
+    if (viewModel == null) {
+      throw FlutterError(
+        'No ViewModelProvider<$T> found in context.\n'
+        'Make sure this widget is under a ViewModelBuilder<$T> in the widget tree.',
+      );
+    }
+
+    return viewModel;
   }
 }
 
 extension ViewModelProviderExtension on BuildContext {
+  T? maybeViewModel<T extends ViewModel>() {
+    return ViewModelProvider.maybeOf<T>(this);
+  }
+
   T viewModel<T extends ViewModel>() {
     return ViewModelProvider.of<T>(this);
   }
